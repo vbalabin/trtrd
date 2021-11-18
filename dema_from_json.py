6131bfd81054dbef5d62f8719df352d00ce0aeef
balabin <balabin@noreply.com>
2021-11-18 21:14:56 +0400

    #3

--- dema_from_json.py
+++ dema_from_json.py
@@ -1,8 +1,25 @@
-from jsondata import load_json_to_dict
+import pandas as pd
+import binance as bn
 from datetime import datetime
 import matplotlib.pyplot as plt
-import pandas as pd
-plt.style.use("fivethirtyeight")
+from jsondata import save_to_json
+from jsondata import load_json_to_dict
+from binance.client import Client
+from credentials import config
+# plt.style.use("fivethirtyeight")
+
+SYM = "ETHUSDT"
+PATH = "klines.json"
+API_KEY = config.api_key
+SECRET_KEY = config.secret_key
+DEMA_SHORT = 20
+DEMA_LONG = 50
+
+def download():
+    client = bn.Client(API_KEY, SECRET_KEY)
+    klines = client.get_historical_klines(SYM, Client.KLINE_INTERVAL_1HOUR, "10 day ago UTC")
+    save_to_json(klines, PATH)
+
 
 def convert_json_to_dicts(path):
     klines = list()
@@ -18,26 +35,29 @@ def convert_json_to_dicts(path):
         klines.append(kd)
     return klines
 
-df = pd.DataFrame(convert_json_to_dicts("klines.json"))
-format = '%Y-%m-%d %H:%M:%S'
-df = df.set_index(pd.DatetimeIndex(df['DateTime']))
+def calculate_n_show():
+    df = pd.DataFrame(convert_json_to_dicts("klines.json"))
+    format = '%Y-%m-%d %H:%M:%S'
+    df = df.set_index(pd.DatetimeIndex(df['DateTime']))
 
-print(df)
+    print(df)
 
-def create_DEMA(df, time_period, column):
-    EMA = df[column].ewm(span=time_period, adjust=False).mean()
-    DEMA = 2 * EMA - EMA.ewm(span=time_period, adjust=False).mean()
-    return DEMA
+    def create_DEMA(df, time_period, column):
+        EMA = df[column].ewm(span=time_period, adjust=False).mean()
+        DEMA = 2 * EMA - EMA.ewm(span=time_period, adjust=False).mean()
+        return DEMA
 
+    df["DEMA_short"] = create_DEMA(df, DEMA_SHORT, "Close")
+    df["DEMA_long"] = create_DEMA(df, DEMA_LONG, "Close")
 
+    col_list = ["DEMA_short", "DEMA_long", "Close"]
+    df[col_list].plot(figsize=(12.2, 6.4))
+    plt.title("Close price")
+    plt.ylabel("y label")
+    plt.xlabel("x label")
+    plt.show()
 
-
-df["DEMA_20"] = create_DEMA(df, 20, "Close")
-df["DEMA_50"] = create_DEMA(df, 50, "Close")
-
-col_list = ["DEMA_20", "DEMA_50", "Close"]
-df[col_list].plot(figsize=(12.2, 6.4))
-plt.title("Close price")
-plt.ylabel("y label")
-plt.xlabel("x label")
-plt.show()
+if __name__ == '__main__':
+    download()
+    calculate_n_show()
+    
--- script02.py
+++ script02.py
@@ -1,10 +1,17 @@
 import json
-import binance as bn
 import pprint
-pp = pprint.PrettyPrinter(indent=4)
 from credentials import config
+import binance as bn
+from jsondata import save_to_json
+from binance.client import Client
 
-client = bn.Client(config.api_key, config.secret_key)
-status = client.get_system_status()
+SYM = "ETHUSDT"
+PATH = "klines.json"
+API_KEY = config.api_key
+SECRET_KEY = config.secret_key
+DEMA_SHORT = 20
+DEMA_LONG = 50
 
-pp.pprint(status)
+client = bn.Client(API_KEY, SECRET_KEY)
+klines = client.get_historical_klines(SYM, Client.KLINE_INTERVAL_1HOUR, "10 day ago UTC")
+save_to_json(klines, PATH)
